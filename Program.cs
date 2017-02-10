1508fdd45ac3492a782053f0b7a9a5d26c7d60db
andres.villarroel <andres.villarroel@abila.com>
2017-02-10 17:11:24 -0400

    Added project
    https://www.codeproject.com/Articles/140911/log-net-Tutorial
    
    2017-02-07 14:44 GMT-04:00 Andres Villarroel Mayorga
    <camusdeacuariogt@gmail.com>:
    https://www.youtube.com/watch?v=2lAdQ_QwNww&feature=youtu.be

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+
+[assembly: log4net.Config.XmlConfigurator(Watch = true)]
+
+namespace Log4NetSample
+{
+    class Program
+    {
+
+        //private static readonly log4net.ILog log = log4net.LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+        //.Net 4.5 has new features to do this in a better way.
+        private static readonly log4net.ILog log = LogHelper.GetLogger();
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Getting started with Log4Net");
+            log.Error("This is an error message");
+            Console.ReadLine();
+        }
+    }
+}
